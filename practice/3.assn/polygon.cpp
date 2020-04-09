41b66c3cb1a72c88cfeff5184694feba567d3090
Dohyung Kim <shapespeare@snu.ac.kr>
2020-04-09 10:36:14 +0900

    출력값 오버플로우 수정

--- practice/3.assn/polygon.cpp
+++ practice/3.assn/polygon.cpp
@@ -6,18 +6,30 @@ using namespace std;
 uint64_t area(vector<pair<int, int>> &points) {
   // This code has signed overflows. :)
   // Please fix this so it correctly evaluates area..!
-  int64_t total = 0;
+  uint64_t total = 0;
+  uint64_t tdiv = 0;
+  bool odd = 0;
   size_t n = points.size();
 
   for (unsigned i = 0; i < n; i++) {
     unsigned j = (i + 1) % n;
-    int x_i = points[i].first;
-    int y_i = points[i].second;
-    int x_j = points[j].first;
-    int y_j = points[j].second;
-    total += (x_i * (y_j - y_i) - y_i * (x_j - x_i));
+    int64_t x_i = points[i].first;
+    int64_t y_i = points[i].second;
+    int64_t x_j = points[j].first;
+    int64_t y_j = points[j].second;
+    tdiv = x_i * (y_j - y_i) - y_i * (x_j - x_i);
+    if (tdiv%2 == 0)  total += tdiv/2;
+    else{
+      if (odd){
+        total += (tdiv+1)/2;
+        odd = false;
+      }  
+      else{
+        total += (tdiv-1)/2;
+        odd = true;
+      }
+    }
   }
 
-  total /= 2;
   return total;
 }
